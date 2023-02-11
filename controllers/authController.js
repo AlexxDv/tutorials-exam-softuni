78b21f2b7916f25ca5afcd2962b67ec0e3ebdf9c
AlexxDv <alexxdimov@gmail.com>
2023-02-11 15:12:22 +0100

    login and register errors add

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const { body } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 const { register, login } = require("../services/userService");
 const { parseError } = require("../util/parser");
 
@@ -10,45 +10,48 @@ authController.get("/register", (req, res) => {
   res.render("register", { title: "Register Page" });
 });
 
-authController.post("/register", async (req, res) => {
+authController.post(
+  "/register",
   body("username")
     .isLength({ min: 5 })
     .withMessage("Username must be at least 5 characters long")
     .isAlphanumeric()
-    .withMessage("Username must contain only letters and numbers");
+    .withMessage("Username must contain only letters and numbers"),
   body("password")
     .isLength({ min: 5 })
     .withMessage("Password must be at least 5 characters long")
     .isAlphanumeric()
-    .withMessage("Password must contain only letters and numbers");
-  try {
-    if (req.body.username == "" || req.body.password == "") {
-      throw new Error("Please fill out all fields");
+    .withMessage("Password must contain only letters and numbers"),
+  async (req, res) => {
+    try {
+      const { errors } = validationResult(req);
+      if (errors.length > 0) {
+        throw errors;
+      }
+      if (req.body.password != req.body.repass) {
+        throw new Error("Passwords do not match");
+      }
+
+      const token = await register(req.body.username, req.body.password);
+
+      //TODO check assignment to see if register creates session
+      res.cookie("token", token);
+      res.redirect("/"); //TODO replace with redirect view by assignment
+    } catch (error) {
+      console.log(error);
+      const errors = parseError(error);
+
+      //TODO add error display to actual templpate from assignment
+      res.render("register", {
+        title: "Register Page",
+        errors,
+        body: {
+          username: req.body.username,
+        },
+      });
     }
-
-    if (req.body.password != req.body.repass) {
-      throw new Error("Passwords do not match");
-    }
-
-    const token = await register(req.body.username, req.body.password);
-
-    //TODO check assignment to see if register creates session
-    res.cookie("token", token);
-    res.redirect("/"); //TODO replace with redirect view by assignment
-  } catch (error) {
-    console.log(error);
-    const errors = parseError(error);
-
-    //TODO add error display to actual templpate from assignment
-    res.render("register", {
-      title: "Register Page",
-      errors,
-      body: {
-        username: req.body.username,
-      },
-    });
   }
-});
+);
 
 authController.get("/login", (req, res) => {
   //TODO replace with actual view by assignment
