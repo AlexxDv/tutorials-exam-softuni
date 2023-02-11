4bdc15ae5936ae74c5aa21d511706010821d4c3b
AlexxDv <alexxdimov@gmail.com>
2023-02-11 14:32:05 +0100

    add login and register

--- middlewares/session.js
+++ middlewares/session.js
@@ -8,6 +8,7 @@ module.exports = () => (req, res, next) => {
       const userData = verifyToken(token);
        console.log("Read seccessful, user", userData.username);
        req.user = userData
+       res.locals.username = userData.username // TODO to add this in the exam 
     } catch (err) {
       console.log("Invalid token");
       res.clearCookie("token");
